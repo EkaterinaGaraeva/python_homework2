cc3916577ea85fcb5447ed1a3427487473279c6c
Ваше имя <garaevaea@mail.ru>
2022-05-26 19:03:09 +0300

    Changed task003

--- task003.py
+++ task003.py
@@ -2,11 +2,13 @@
 # и минимальным значением дробной части элементов. 
 # Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
+'''
 def find_difference(list_of_numbers):
     list_of_fractional_part = []
     for i in list_of_numbers:
         #if (i - int(i)) != 0:
-        if (type(i) == float):
+        #if (type(i) == float):
+        if (isinstance(i, float) == True):
             list_of_fractional_part.append(i - int(i))
     max = list_of_fractional_part[0]
     min = list_of_fractional_part[0]
@@ -20,5 +22,34 @@ def find_difference(list_of_numbers):
     min = round(min, 2)
     print(f'Разница между максимальным значением дробной части {max} и минимальным значением дробной части {min} равна {round((max - min), 2)}')
 
+def find_difference2(list_of_numbers):
+    max = list_of_numbers[0] - int(list_of_numbers[0])
+    min = list_of_numbers[0] - int(list_of_numbers[0])
+    for j in list_of_numbers:
+        if (max < (j - int(j))) and (isinstance(j, float) == True):
+            max = j - int(j)
+    for k in list_of_numbers:
+        if (min > (k - int(k))) and (isinstance(k, float) == True):
+            min = k - int(k)
+    max = round(max, 2)
+    min = round(min, 2)
+    print(f'Разница между максимальным значением дробной части {max} и минимальным значением дробной части {min} равна {round((max - min), 2)}')
+'''
+
+def find_difference3(list_of_numbers):
+    max = list_of_numbers[0] % 1
+    min = list_of_numbers[0] % 1
+    for j in list_of_numbers:
+        if (max < (j % 1)) and (isinstance(j, float) == True):
+            max = j % 1
+    for k in list_of_numbers:
+        if (min > (k % 1)) and (isinstance(k, float) == True):
+            min = k % 1
+    max = round(max, 2)
+    min = round(min, 2)
+    print(f'Разница между максимальным значением дробной части {max} и минимальным значением дробной части {min} равна {round((max - min), 2)}')
+
 new_list = [1.1, 1.2, 3.1, 5, 10.01]
-find_difference(new_list)
+#find_difference(new_list)
+#find_difference2(new_list)
+find_difference3(new_list)
